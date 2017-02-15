9a4600197af3624159f1fa1b682fab123185a039
HoverEagle <jeremyxu89@gmail.com>
2017-02-14 23:31:38 -0500

    Add shop-stuff section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,15 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { NavComponent } from './components/nav/nav.component';
+import { ShopHomeComponent } from './components/shop/shop-home/shop-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
-    NavComponent
+    NavComponent,
+    ShopHomeComponent,
+    ShopHomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/shop/shop-home/shop-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cohab-shopHome',
+  templateUrl: './shop-home.component.html',
+  styleUrls: ['./shop-home.component.css']
+})
+export class ShopHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
