952718c8902bf43c8eec06fb40c9b3c89a5ce7df
HoverEagle <jeremyxu89@gmail.com>
2017-02-21 20:03:34 -0500

    Carousel nav bar style applied.
    When applying carousel styles, the target class has to be added with
    “owl-theme”.

--- src/app/components/journal/journal-service.service.ts
+++ src/app/components/journal/journal-service.service.ts
@@ -67,20 +67,18 @@ export class JournalService {
             $('.owl-carousel').owlCarousel({
               loop:true,
               margin:10,
-              nav: true,
               dots:true,
+              nav: true,
+              
               responsive:{
                   0:{
                       items:1,
-                      nav:true
                   },
                   600:{
-                      items:2,
-                      nav:false
+                      items:2,                     
                   },
                   1050:{
                       items:3,
-                      nav:true,
                     }
                 }, 
             })
